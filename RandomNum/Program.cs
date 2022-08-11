3ec9b20a6c670105ede5dc64681bb1165f19b1d4
Caleb Archer <calebandcorey@gmail.com>
2022-08-11 20:42:45 +1200

    Add console input and .gitignore

--- RandomNum/Program.cs
+++ RandomNum/Program.cs
@@ -11,11 +11,26 @@ namespace RandomNum
         {
             var rng = new RandomNumberGenerator();
             var hashTable = new Dictionary<int, int>();
+            int count = 0;
+            bool validInput = false;
 
-            for (int i = 0; i < 1000000000; i++)
+            Console.WriteLine("This is a random number generator (not truly random). It will generate a number between 0 and 10 a certain number of times.");
+            Console.Write("Please enter the number of random numbers you'd like to generate: ");
+
+            while (validInput is false)
+            {
+                string inputRaw = Console.ReadLine();
+                validInput = int.TryParse(inputRaw, out count);
+
+                if (validInput is false)
+                    Console.WriteLine("That's not a valid integer! Please enter a proper number: ");
+            }
+
+            for (int i = 0; i < count; i++)
             {
                 int randomNum = rng.NextInt(11);
 
+                // If number already exists in hash table, increase its occurrence count
                 if (hashTable.ContainsKey(randomNum))
                     hashTable[randomNum]++;
                 else
